61c10f6d8e98ed56e9d2123d1e62d94c62a547e3
itrin9634 <itrin9634@naver.com>
2023-08-03 23:32:24 +0900

    refactoring by lombok

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 
 @Entity // 이걸 해줘야 DB가 해당객체를 인식가능
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id// 대표값을 지정
     @GeneratedValue //1,2,3 자동생성 어노테이션
@@ -16,18 +21,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }

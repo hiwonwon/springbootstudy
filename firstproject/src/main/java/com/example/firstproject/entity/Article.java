025caadcb378fcfb79b65cd070d285a16aa2718b
itrin9634 <itrin9634@naver.com>
2023-08-14 20:57:58 +0900

    data inquiry with JPA

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 
 @Entity // 이걸 해줘야 DB가 해당객체를 인식가능
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor //디폴트 생성자 추가
 public class Article {
     @Id// 대표값을 지정
     @GeneratedValue //1,2,3 자동생성 어노테이션

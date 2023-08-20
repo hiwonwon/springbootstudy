72a37a9650d99843270b226f7f99c38a7bd86062
hiwonwon <hyewon9913@gmail.com>
2023-08-20 22:55:58 +0900

    modify data

---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id,title,content) VALUES (1,'aaa','1111')
+INSERT INTO article(id,title,content) VALUES (2,'bbb','2222')
+INSERT INTO article(id,title,content) VALUES (3,'cccc','3333')
